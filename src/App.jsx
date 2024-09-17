ac46383a3180e045ebb0b964ba4edbb5ba3157e7
LerichO <Lerich.Osay@UTDallas.edu>
2024-09-17 18:19:17 -0500

    boilerplat

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({ firstName: '', lastName: '', netID: '' })
+  const [submitted, setSubmitted] = useState(false);
+  const [member, setMember] = useState(true);
+
+  const checkMembership = async (e) => {
+    e.preventDefault()
+    setSubmitted(true);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-full'>
+      <h1 className='flex flex-wrap m-4'>Check SHPE UTD Membership</h1>
+      <div className='flex h-96 flex-col'>
+        <form className='flex flex-col justify-center items-center' onSubmit={checkMembership}>
+          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.firstName} onChange={(e) => { setUser({ ...user, firstName: e.target.value }); setSubmitted(false); }} placeholder="First Name" id="firstName" name="firstName" />
+          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.lastName} onChange={(e) => { setUser({ ...user, lastName: e.target.value }); setSubmitted(false); }} placeholder="Last Name" id="lastName" name="lastName" />
+          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.netID} onChange={(e) => { setUser({ ...user, netID: e.target.value }); setSubmitted(false); }} placeholder="NetID (ie ABC123456)" id="netID" name="netID" />
+          <button className='w-40' type="submit">Check Member</button>
+        </form>
+        {submitted && <h2 className='text-5xl'>{member ? "✅" : "❌"}</h2>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
