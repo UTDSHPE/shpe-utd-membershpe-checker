d37689134af15ef109d0e650fe4172440836a904
LerichO <Lerich.Osay@UTDallas.edu>
2024-09-21 20:09:51 -0500

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,16 @@ function App() {
   }
 
   return (
-    <div className='h-full'>
-      <h1 className='flex flex-wrap m-4'>Check SHPE UTD Membership</h1>
+    <div className='h-full flex flex-col justify-center items-center'>
+      <h1 className='flex flex-wrap my-8'>Check SHPE UTD Membership</h1>
       <div className='flex h-96 flex-col'>
         <form className='flex flex-col justify-center items-center' onSubmit={checkMembership}>
           <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.firstName} onChange={(e) => { setUser({ ...user, firstName: e.target.value }); setSubmitted(false); }} placeholder="First Name" id="firstName" name="firstName" />
           <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.lastName} onChange={(e) => { setUser({ ...user, lastName: e.target.value }); setSubmitted(false); }} placeholder="Last Name" id="lastName" name="lastName" />
           <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.netID} onChange={(e) => { setUser({ ...user, netID: e.target.value }); setSubmitted(false); }} placeholder="NetID (ie ABC123456)" id="netID" name="netID" />
-          <button className='w-40' type="submit">Check Member</button>
+          <button className='w-40 rounded-full' type="submit">Check Member</button>
         </form>
-        {submitted && <h2 className='text-5xl'>{member ? "✅" : "❌"}</h2>}
+        {submitted && <h2 className='text-5xl my-8'>{member ? "✅" : "❌"}</h2>}
       </div>
     </div>
   )
