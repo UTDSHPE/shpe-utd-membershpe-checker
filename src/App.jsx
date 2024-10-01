571bee83df2b6e23260adf6f1056d7205c081ad1
LerichO <Lerich.Osay@UTDallas.edu>
2024-10-01 12:36:22 -0500

    merge con

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState({ firstName: '', lastName: '', netID: '' })
+  const [user, setUser] = useState({ name: '', netID: '' })
   const [submitted, setSubmitted] = useState(false);
   const [member, setMember] = useState(true);
   const [errorMsg, setErrorMsg] = useState('');
@@ -13,14 +13,16 @@ function App() {
   const checkMembership = async (e) => {
     e.preventDefault();
 
-    if (user.firstName && user.lastName && user.netID) {
+    console.log(user.name, user.netID)
+
+    if (user.name && user.netID) {
       try {
-        // normalize netId to 'abc123456' format
-        const response = await axios.post(`${import.meta.env.VITE_APP_SERVER_URL}/api/members`, { ...user, netID: user.netID.split(6)[0].toLowerCase() });
-        setMember(response.data.isValid);
+        // normalize strings before sending
+        const response = await axios.post(`${import.meta.env.VITE_APP_SERVER_URL}/api/membershpe`, { name: user.name.toLowerCase(), netID: user.netID.toLowerCase() });
+        setMember(response.data.member);
       } catch (error) {
         console.log(error)
-        setErrorMsg(error.response ? error.response.data.error : error.message);
+        setErrorMsg(error.response.data.error ? error.response.data.error : error.message);
         setMember(false);
       }
     }
@@ -37,8 +39,7 @@ function App() {
       <h1 className='flex flex-wrap my-8 text-5xl'>Check SHPE UTD Membership</h1>
       <div className='flex h-96 flex-col'>
         <form className='flex flex-col justify-center items-center' onSubmit={checkMembership}>
-          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.firstName} onChange={(e) => { setUser({ ...user, firstName: e.target.value }); setSubmitted(false); setErrorMsg(''); }} placeholder="First Name" id="firstName" name="firstName" />
-          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.lastName} onChange={(e) => { setUser({ ...user, lastName: e.target.value }); setSubmitted(false); setErrorMsg(''); }} placeholder="Last Name" id="lastName" name="lastName" />
+          <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.name} onChange={(e) => { setUser({ ...user, name: e.target.value }); setSubmitted(false); setErrorMsg(''); }} placeholder="Name" id="Name" name="Name" />
           <input className="mb-3 p-3 rounded-full w-72 text-md" type="text" value={user.netID} onChange={(e) => { setUser({ ...user, netID: e.target.value }); setSubmitted(false); setErrorMsg(''); }} placeholder="NetID (ie ABC123456)" id="netID" name="netID" />
           <button className='w-40 rounded-full' type="submit">Check Member</button>
         </form>
